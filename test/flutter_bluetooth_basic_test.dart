063f21ba57cceffd7967d3fce1f55e707230a42a
Andrey <ushakov.andrey.34@gmail.com>
2020-01-15 00:03:37 +0100

    init

---
+++ test/flutter_bluetooth_basic_test.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/services.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_bluetooth_basic/flutter_bluetooth_basic.dart';
+
+void main() {
+  const MethodChannel channel = MethodChannel('flutter_bluetooth_basic');
+
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  setUp(() {
+    channel.setMockMethodCallHandler((MethodCall methodCall) async {
+      return '42';
+    });
+  });
+
+  tearDown(() {
+    channel.setMockMethodCallHandler(null);
+  });
+
+  test('getPlatformVersion', () async {
+    expect(await FlutterBluetoothBasic.platformVersion, '42');
+  });
+}
